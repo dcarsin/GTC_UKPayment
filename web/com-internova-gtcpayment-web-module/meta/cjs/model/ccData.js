c6741cdd27d547152cd115516b77e8ded0bda73f
dcarsin <dcarsin@byui.edu>
2023-03-08 00:55:20 -0300

    test after adding dependencies

--- web/com-internova-gtcpayment-web-module/meta/cjs/model/ccData.js
+++ web/com-internova-gtcpayment-web-module/meta/cjs/model/ccData.js
@@ -1,9 +1,1 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ccData = void 0;
-var ccData = /** @class */ (function () {
-    function ccData() {
-    }
-    return ccData;
-}());
-exports.ccData = ccData;
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.ccData=void 0;var ccData=function(){return function ccData(){}}();exports.ccData=ccData;
